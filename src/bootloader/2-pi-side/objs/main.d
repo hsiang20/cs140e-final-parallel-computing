b35e4341f7f457bf5a9be43ccdf7a227b297e3e4
張家翔 <hsiang@DNa80e6c0.SUNet>
2025-03-05 21:53:57 -0800

    minor

---
+++ src/bootloader/2-pi-side/objs/main.d
@@ -0,0 +1,26 @@
+objs/main.o: main.c \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/rpi.h \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/libc/demand.h \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/gpio.h \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/rpi-constants.h \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/your-prototypes.h \
+ get-code.h boot-crc32.h boot-defs.h \
+ /Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/memmap.h
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/rpi.h:
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/libc/demand.h:
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/gpio.h:
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/rpi-constants.h:
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/your-prototypes.h:
+
+get-code.h:
+
+boot-crc32.h:
+
+boot-defs.h:
+
+/Users/hsiang/CS140E/cs140e-final-parallel-computing//libpi/include/memmap.h:
